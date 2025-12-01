2111487937f2bbf412340d0187ac13673d8696cc
iyaaadh-web <iyaaadh@gmail.com>
2025-12-01 14:36:54 +0500

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -40,8 +40,8 @@ export const Navigation = () => {
                     <Image
                         src="/images/type-logo-1.png"
                         alt="Logo"
-                        width={1000}
-                        height={1000}
+                        width={800}
+                        height={800}
                         className="object-contain filter invert brightness-0"
                         priority
                     />
@@ -151,6 +151,7 @@ export const Navigation = () => {
         </nav>
     );
 };
+
 
 
 
